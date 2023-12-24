2adc58ddf5adef538de51b19af84606e736cecfe
DevvKaii <154115677+DevvKaii@users.noreply.github.com>
2023-12-24 17:27:47 +0700

    Update Pembayaran.js

--- src/components/Pembayaran.js
+++ src/components/Pembayaran.js
@@ -11,56 +11,6 @@ const Pembayaran = ({ dataUser, setDataUser }) => {
 
   return (
     <div className="card-content p-4" id="Pembayaran">
-      <div className="row mb-3">
-        <input
-          type="radio"
-          name="pembayaran"
-          id="telkomsel"
-          value="Telkomsel"
-          onChange={handleChange}
-          checked={dataUser.pembayaran === "Telkomsel"}
-        />
-        <div className="col">
-          <label
-            className="pl-3 text-start card-pmbayaran d-flex align-items-center justify-content-start position-relative"
-            htmlFor="telkomsel"
-          >
-            TELKOMSEL
-            <p
-              className="mx-n5 position-absolute top-50 start-100 translate-middle"
-              style={{ width: "120px" }}
-            >
-              {dataUser.harga}
-            </p>
-          </label>
-        </div>
-      </div>
-
-      <div className="row mb-3 ">
-        <input
-          type="radio"
-          name="pembayaran"
-          id="bca"
-          value="BCA"
-          onChange={handleChange}
-          checked={dataUser.pembayaran === "BCA"}
-        />
-        <div className="col ">
-          <label
-            className="pl-3 text-start card-pmbayaran d-flex align-items-center justify-content-start position-relative"
-            htmlFor="bca"
-          >
-            BCA
-            <p
-              className="mx-n5 position-absolute top-50 start-100 translate-middle"
-              style={{ width: "120px" }}
-            >
-              {dataUser.harga}
-            </p>
-          </label>
-        </div>
-      </div>
-
       <div className="row mb-3 ">
         <input
           type="radio"
